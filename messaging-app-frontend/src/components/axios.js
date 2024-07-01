e7ac3e5ad021273e5f3477c9b4e1d09fd594c48a
Pratyusha <167186378+pratyusha-ds@users.noreply.github.com>
2024-07-01 23:57:43 +0200

    Changed backend URL in axios

--- messaging-app-frontend/src/components/axios.js
+++ messaging-app-frontend/src/components/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://mern-messaging-app-k1lv.onrender.com/",
 });
 export default instance;
